2999e4dc3bd01fd6d64c0c1cd2b92078722174b3
JanNeoV <142179566+JanNeoV@users.noreply.github.com>
2024-09-01 11:09:02 +0200

    blubb

--- etl/ismene/models/staging/blubb.sql
+++ etl/ismene/models/staging/blubb.sql
@@ -1,12 +1,5 @@
     SELECT 
-    DISTINCT geo
-    , sex,obs_value, age
-        --obs_value, geo
+ *
     FROM
-        {{ source('Source', 'first_marriage_rates_by_age_and_sex') }}
-        WHERE time_period = 2010
-        AND geo = 'DE_TOT'
-        --AND geo = 'DE'
-        ORDER BY obs_value DESC
-        --AND age = 'Y35-39' OR age = 'Y30-34'
+        {{ source('Source', 'crude_divorce_rate') }}
 
